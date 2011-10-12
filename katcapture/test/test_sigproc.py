9fcffa7de2ef86bfe7628bbd9856ce3fe79af9dc
Simon Ratcliffe <sratcliffe@ska.ac.za>
2011-10-12 14:26:57 +0200

    Fixed simple sigproc tests.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katcapture/trunk@11398 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- katcapture/test/test_sigproc.py
+++ katcapture/test/test_sigproc.py
@@ -11,20 +11,17 @@ class SigProcTestCases(unittest.TestCase):
         """Create the basic environment, along with test vectors."""
         self.scale_factor = 2
         self.n_ts = 10
-        self.n_chans = 1024
-        self.n_bls = 144
-        self.spike_channel = 123
-        self.spike_bls = 13
-         # the channel and baseline into which to insert rfi spikes
-        self.rfi_threshold = 3
-         # no of sigma for rfi threshold
+        self.n_chans = 512
+        self.n_ants = 4
+        self.n_bls = 2 * self.n_ants * (self.n_ants+1)
 
-        self.data = (np.random.random((self.n_ts, self.n_chans, self.n_bls,2)) * 98).astype(np.int32).astype(np.float32)
+        self.spike_channel = np.random.randint(self.n_chans)
+        self.spike_bls = np.random.randint(self.n_bls)
+         # the channel and baseline into which to insert rfi spikes
+
+        self.data = (np.random.random((self.n_ts, self.n_chans, self.n_bls,2)) + 100).astype(np.int32)
          # produce some random data
         self.data_copy = self.data.copy()
-        self.data_copy_complex = self.data.copy().view(np.complex64).squeeze()
-        #self.data_ones = np.ones((self.n_ts,self.n_chans,self.n_bls), dtype=np.complex64)
-        #self.data_short = np.ones((self.n_ts,self.n_chans), dtype=np.complex64)
 
         sigproc.ProcBlock.history = self.data
         sigproc.ProcBlock.current = self.data[0]
@@ -36,7 +33,7 @@ class SigProcTestCases(unittest.TestCase):
 
     def testBlsOrdering(self):
         """Check that CorrProdRef is built correctly."""
-        pc = sigproc.ProcBlock(bls_ordering=None)
+        pc = sigproc.ProcBlock(n_ants=self.n_ants)
         self.assertEqual(len(pc.cpref._id_to_real), self.n_bls)
 
     def testBasicAssignment(self):
@@ -46,7 +43,7 @@ class SigProcTestCases(unittest.TestCase):
 
     def testScaleDataType(self):
         """Test acceptable data type for scale."""
-        data_wrong_type = np.ones((self.n_ts,self.n_chans,self.n_bls,2), dtype=np.int32)
+        data_wrong_type = np.ones((self.n_ts,self.n_chans,self.n_bls,2), dtype=np.float32)
         sigproc.ProcBlock.history = data_wrong_type
         sigproc.ProcBlock.current = data_wrong_type[0]
         sc = sigproc.Scale(self.scale_factor)
@@ -57,6 +54,7 @@ class SigProcTestCases(unittest.TestCase):
         sc = sigproc.Scale(self.scale_factor)
         sc.proc()
         np.testing.assert_equal(self.data[0], self.data_copy[0] / self.scale_factor)
+        self.assertNotEqual(self.data[0].dtype, np.float32)
 
     def testVanVleck(self):
         """Not implemented yet..."""
@@ -64,30 +62,17 @@ class SigProcTestCases(unittest.TestCase):
 
     def testRFIThresholdDataType(self):
         """Test acceptable data type for rfi threshold."""
-        rfi = sigproc.RFIThreshold(self.rfi_threshold)
+        rfi = sigproc.RFIThreshold2()
+        data_wrong_type = np.ones((self.n_ts,self.n_chans,self.n_bls,2), dtype=np.int32)
+        sigproc.ProcBlock.history = data_wrong_type
+        sigproc.ProcBlock.current = data_wrong_type[0]
         self.assertRaises(TypeError, rfi.proc)
 
-    def testRFIThreshold(self):
-        """Test simple RFI thresholding..."""
-        bls_std = np.std(abs(self.data_copy_complex[1:,self.spike_channel,self.spike_bls]))
-        bls_mean = np.mean(abs(self.data_copy_complex[1:,self.spike_channel,self.spike_bls]))
-        self.data_spike_large = self.data_copy_complex.copy()
-        self.data_spike_large[0,self.spike_channel,self.spike_bls] = bls_mean + (self.rfi_threshold*1.01) * bls_std
-        self.data_spike_small = self.data_copy_complex.copy()
-        self.data_spike_small[0,self.spike_channel,self.spike_bls] = bls_mean + (self.rfi_threshold*0.99) * bls_std
-         # very simple test vectors for basic rfi thresholding
-
-         # test for detection
-        sigproc.ProcBlock.history = self.data_spike_large
-        sigproc.ProcBlock.current = self.data_spike_large[0]
-        rfi = sigproc.RFIThreshold(self.rfi_threshold)
+    def testRFIThreshold2(self):
+        """Test simple RFI Thresholding..."""
+        data = (np.random.random((self.n_chans, self.n_bls)) + 100).astype(np.complex64)
+        data[self.spike_channel, self.spike_bls] += 100
+        sigproc.ProcBlock.current = data
+        rfi = sigproc.RFIThreshold()
         flags = np.unpackbits(rfi.proc()).reshape(self.n_chans, self.n_bls)
-        self.assertEqual(flags[self.spike_channel, self.spike_bls], 1)
-
-         # test for no detection
-        sigproc.ProcBlock.history = self.data_spike_small
-        sigproc.ProcBlock.current = self.data_spike_small[0]
-        flags = np.unpackbits(rfi.proc()).reshape(self.n_chans, self.n_bls)
-        self.assertEqual(flags[self.spike_channel, self.spike_bls], 0)
-
-
+        self.assertEqual(flags[self.spike_channel, self.spike_bls],1)
