5151dd555be0b9175ae5252d73948ff2bc79ff6d
Laura Richter <laura@ska.ac.za>
2013-10-17 10:11:30 +0200

    Changes to the katcp2spead bridge - Transmission now occurs in a separate transmitter thread, so that   the sensor stream can be sent to multiple IPs and posrs, if desired. - Sensors are all transmitted the same time period. Event sensors   are an exception, they are transmitted when the event occurs.

--- scripts/katcp2spead.py
+++ scripts/katcp2spead.py
@@ -12,17 +12,19 @@ import katconf
 import katcorelib
 from katsdpingest.katcp2spead import Katcp2SpeadDeviceServer
 
+INTTIME = 0.5
+
 sensors = [
     ('dbe7_target', 'event', ''),
-    ('ant1_activity', 'event', ''),
+#    ('ant1_activity', 'event', ''),
     ('ant2_activity', 'event', ''),
-    ('ant3_activity', 'event', ''),
+#    ('ant3_activity', 'event', ''),
     ('ant4_activity', 'event', ''),
-    ('ant5_activity', 'event', ''),
-    ('ant6_activity', 'event', ''),
-    ('ant7_activity', 'event', ''),
+#    ('ant5_activity', 'event', ''),
+#    ('ant6_activity', 'event', ''),
+#    ('ant7_activity', 'event', ''),
 #    ('ant2_pos_actual_scan_azim', 'period', '0.4'),
-#    ('ant2_pos_actual_scan_elev', 'period', '0.4'),
+    ('ant2_pos_actual_scan_elev', 'period', '0.4')
 ]
 
 # Parse command-line options
@@ -59,8 +61,9 @@ logger = logging.getLogger("kat.katcp2spead")
 # Get host object through which to access system sensors
 kat = katcorelib.tbuild(system=opts.system)
 # Create device server that is main bridge between KATCP and SPEAD
-server = Katcp2SpeadDeviceServer(kat, sensors, opts.spead_host, opts.spead_port,
-                                 host=opts.ctl_host, port=opts.ctl_port)
+server = Katcp2SpeadDeviceServer(kat, sensors, INTTIME, spead_host=opts.spead_host, 
+                                 spead_port=opts.spead_port, host=opts.ctl_host, 
+                                 port=opts.ctl_port)
 server.set_restart_queue(Queue.Queue())
 # Spawn new thread to handle KATCP requests to device server
 server.start()
