2eb45f41e5afb0e9380c3cf77f1e7b59a5cb2ac5
Bruce Merry <bmerry@ska.ac.za>
2014-10-30 16:55:37 +0200

    Make mako templates for device kernels install

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup (
     description = "Karoo Array Telescope Data Capture",
     author = "Simon Ratcliffe",
     packages = find_packages(),
-    package_data={'': ['conf/*']},
+    package_data={'': ['conf/*', 'ingest_kernels/*.mako']},
     include_package_data = True,
     scripts = [
         "scripts/ingest.py",
