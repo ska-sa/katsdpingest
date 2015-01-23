0d3a17bff76260d942529e303b7253ec1defb6c5
Bruce Merry <bmerry@ska.ac.za>
2015-01-23 15:46:42 +0200

    Write test for _TimeAverage class

--- katsdpingest/test/test_ingest_threads.py
+++ katsdpingest/test/test_ingest_threads.py
@@ -3,7 +3,60 @@
 import numpy as np
 from katsdpingest import ingest_threads
 from katsdpsigproc.test.test_accel import device_test
+import collections
 import unittest
+import mock
+from nose.tools import *
+
+FakeValue = collections.namedtuple('FakeValue', ['value'])
+
+class TestTimeAverage(object):
+    def setup(self):
+        self.cbf_attr = {
+            'n_accs': FakeValue(123456),
+            'n_chans': FakeValue(4096),
+            'int_time': FakeValue(0.75)
+        }
+        self.input_interval = 123456 * 4096 * 2
+
+    def test_constructor(self):
+        avg = ingest_threads._TimeAverage(self.cbf_attr, 2.0)
+        assert_equal(2.25, avg.int_time)
+        assert_equal(3, avg.ratio)
+        assert_equal(avg.ratio * self.input_interval, avg.interval)
+        assert_is(None, avg._start_ts)
+        assert_equal([], avg._ts)
+
+    def make_ts(self, idx):
+        if isinstance(idx, list):
+            return [self.make_ts(x) for x in idx]
+        else:
+            return 1000000000 + idx * self.input_interval
+
+    def test_add_timestamp(self):
+        avg = ingest_threads._TimeAverage(self.cbf_attr, 2.0)
+        avg.flush = mock.Mock(name='flush', spec_set=avg.flush)
+        avg.add_timestamp(self.make_ts(0))
+        avg.add_timestamp(self.make_ts(2))
+        avg.add_timestamp(self.make_ts(1))  # Test time reordering
+        assert not avg.flush.called
+
+        avg.add_timestamp(self.make_ts(4))  # Skip first packet in the group
+        avg.flush.assert_called_once_with(self.make_ts([0, 2, 1]))
+        avg.flush.reset_mock()
+        assert_equal(self.make_ts(3), avg._start_ts)
+        assert_equal([self.make_ts(4)], avg._ts)
+
+        avg.add_timestamp(self.make_ts(12))  # Skip some whole groups
+        avg.flush.assert_called_once_with(self.make_ts([4]))
+        avg.flush.reset_mock()
+        assert_equal(self.make_ts(12), avg._start_ts)
+        assert_equal([self.make_ts(12)], avg._ts)
+
+        avg.finish()
+        avg.flush.assert_called_once_with(self.make_ts([12]))
+        assert_is(None, avg._start_ts)
+        assert_equal([], avg._ts)
 
 class TestCBFIngest(unittest.TestCase):
     @device_test
