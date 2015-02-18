63358cd247609dd0fe04f34bc07ba8e57266da91
Bruce Merry <bmerry@ska.ac.za>
2015-02-18 11:57:54 +0200

    Adjust autotuning script to make the base image a command-line option

--- scripts/autotune_mkimage.sh
+++ scripts/autotune_mkimage.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Create a derived image from the katsdpingest base image that contains
-# autotuning results. The ska_sa/katsdpingest image must be available.
+# autotuning results.
 set -e
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <image-name>" 1>&2
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <image-name> <base-image>" 1>&2
     exit 1
 fi
 
@@ -12,7 +12,8 @@ args=()
 for i in /dev/nvidia*; do
     args+=(--device $i:$i)
 done
-container="$(docker run -d "${args[@]}" ska_sa/katsdpingest ingest_autotune.py)"
+docker pull "$2"
+container="$(docker run -d "${args[@]}" "$2" ingest_autotune.py)"
 if [ -z "$container" ]; then
     echo "did not get container name" 1>&2
     exit 1
