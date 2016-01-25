a2750b50dcc4d83763888333100b9cd289372961
Bruce Merry <bmerry@ska.ac.za>
2016-01-25 12:41:54 +0200

    Add Jenkins script to do autotuning

---
+++ jenkins-autotune.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+if [ "$#" -ne 1 ]; then
+    echo "Usage: jenkins-autotune.sh <gpu-tag>" 1>&2
+    exit 1
+fi
+GPU="$1"
+LABEL="${GIT_BRANCH#origin/}"
+if [ "$LABEL" = "master" ]; then
+    LABEL=latest
+fi
+IMAGE="$DOCKER_REGISTRY/katsdpingest_$GPU:$LABEL"
+BASE_IMAGE="$DOCKER_REGISTRY/katsdpingest:$LABEL"
+docker pull "$BASE_IMAGE"
+scripts/autotune_mkimage.py "$IMAGE" "$BASE_IMAGE"
+docker push "$IMAGE"
