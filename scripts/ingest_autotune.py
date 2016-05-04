c6e4b2234f6dc0a8196fbe05ff286a3503baf1e2
Bruce Merry <bmerry@ska.ac.za>
2016-05-04 15:52:43 +0200

    Rename ingest_thread -> ingest_session in autotune script

--- scripts/ingest_autotune.py
+++ scripts/ingest_autotune.py
@@ -5,20 +5,20 @@ Makes autotuning happen for common configurations and for all discovered
 devices, so that a subsequent run will not have to wait for autotuning.
 """
 import logging
-from katsdpingest import ingest_threads
+from katsdpingest import ingest_session
 from katsdpsigproc import accel
 
 def autotune_device(device):
     context = device.make_context()
-    tune_antennas = ingest_threads.CBFIngest.tune_antennas
-    tune_channels = ingest_threads.CBFIngest.tune_channels
+    tune_antennas = ingest_session.CBFIngest.tune_antennas
+    tune_channels = ingest_session.CBFIngest.tune_channels
 
     # The tuning parameters are independent, so we just need to tune along a
     # line in each dimension, rather than the Cartesian product.
     for antennas in tune_antennas:
-        ingest_threads.CBFIngest.create_proc_template(context, antennas, tune_channels[0])
+        ingest_session.CBFIngest.create_proc_template(context, antennas, tune_channels[0])
     for channels in tune_channels:
-        ingest_threads.CBFIngest.create_proc_template(context, tune_antennas[0], channels)
+        ingest_session.CBFIngest.create_proc_template(context, tune_antennas[0], channels)
 
 def main():
     logging.getLogger('katsdpsigproc.tune').setLevel(logging.INFO)
