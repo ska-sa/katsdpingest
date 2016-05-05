4f208149c43efdcbda3e0d8e91403347c5d956af
Bruce Merry <bmerry@ska.ac.za>
2016-05-05 16:27:22 +0200

    Fix flake8/pylint warnings

--- digitiser_capture/digitiser_capture.py
+++ digitiser_capture/digitiser_capture.py
@@ -11,8 +11,9 @@ import sys
 import tempfile
 import contextlib
 
+
 def option_pair(base_type):
-    def fn(value):
+    def callback(value):
         parts = value.split(',', 1)
         if len(parts) == 1:
             out = base_type(parts[0])
@@ -21,7 +22,7 @@ def option_pair(base_type):
             out0 = base_type(parts[0])
             out1 = base_type(parts[1])
             return out0, out1
-    return fn
+    return callback
 
 
 def parse_args():
@@ -29,11 +30,14 @@ def parse_args():
     parser.add_argument('address', type=option_pair(str), help='Multicast groups')
     parser.add_argument('output', type=str, help='Output file')
     parser.add_argument('--port', type=option_pair(int), default='7148', help='UDP ports')
-    parser.add_argument('--interface', type=option_pair(str), default='p5p1,p4p1', help='Network interfaces')
-    parser.add_argument('--tmpdir', type=option_pair(str), default='/mnt/ramdisk0,/mnt/ramdisk1', help='Temporary directories (should be ramdisks)')
+    parser.add_argument('--interface', type=option_pair(str), default='p5p1,p4p1',
+                        help='Network interfaces')
+    parser.add_argument('--tmpdir', type=option_pair(str), default='/mnt/ramdisk0,/mnt/ramdisk1',
+                        help='Temporary directories (should be ramdisks)')
     parser.add_argument('-s', '--seconds', type=float, default=5, help='Length of capture')
     parser.add_argument('--heaps', type=int, help='Maximum number of heaps to convert')
-    parser.add_argument('--non-icd', action='store_true', help='Assume digitiser is not ICD compliant')
+    parser.add_argument('--non-icd', action='store_true',
+                        help='Assume digitiser is not ICD compliant')
     return parser.parse_args()
 
 
@@ -61,9 +65,9 @@ def main():
             tcpdump.append(subprocess.Popen(
                 ['hwloc-bind', 'os={}'.format(args.interface[i]), '--',
                  'tcpdump', '-i', args.interface[i], '-n', '-p', '-q', '-s', '8192', '-B', '4096',
-                 'ip proto \\udp and dst port {} and dst host {}'.format(args.port[i], args.address[i]),
-                 '-w', pcap_file[i].name
-                ]))
+                 'ip proto \\udp and dst port {} and dst host {}'.format(
+                     args.port[i], args.address[i]),
+                 '-w', pcap_file[i].name]))
         time.sleep(args.seconds)
         for i in range(2):
             tcpdump[i].send_signal(signal.SIGINT)
