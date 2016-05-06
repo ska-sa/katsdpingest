438c6e76b8619d179584020cd339fdfa13f97f1a
Bruce Merry <bmerry@ska.ac.za>
2016-05-06 14:13:51 +0200

    Add --interface option to bf_ingest.py
    This allows the interface used for multicast subscription to be
    overridden.

--- scripts/bf_ingest.py
+++ scripts/bf_ingest.py
@@ -29,6 +29,7 @@ def main():
     parser.add_argument('--file-base', default='.', type=str, help='base directory into which to write HDF5 files', metavar='DIR')
     parser.add_argument('--buffer', action='store_true', help='Buffer all data in memory during capture')
     parser.add_argument('--affinity', type=spead2.parse_range_list, help='List of CPUs to which to bind threads', metavar='CPU,CPU,CPU')
+    parser.add_argument('--interface', type=str, help='Network interface for multicast subscription')
     parser.add_argument('--port', '-p', type=int, default=2050, help='katcp host port')
     parser.add_argument('--host', '-a', type=str, default='', help='katcp host address')
     args = parser.parse_args()
