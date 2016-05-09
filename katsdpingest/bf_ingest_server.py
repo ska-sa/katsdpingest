b34a8ff40205313a9d7096d8ff4cc1adc80234ed
Bruce Merry <bmerry@ska.ac.za>
2016-05-09 12:14:12 +0200

    Add a timestamp_reference attribute to HDF5 beamformer files
    To indicate that timestamps refer to the start of the spectrum, as
    discussed in SR-328.

--- katsdpingest/bf_ingest_server.py
+++ katsdpingest/bf_ingest_server.py
@@ -219,6 +219,7 @@ class _CaptureSession(object):
                 'timestamps',
                 shape=(n_time * len(self._timestamps),),
                 dtype=np.uint64)
+            ds.attrs['timestamp_reference'] = 'start'
             idx = 0
             for timestamp in self._timestamps:
                 ds[idx : idx + n_time] = np.arange(
