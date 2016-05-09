e7818f87f7eb571ea0ebb53d4f087887af9aa1b1
Bruce Merry <bmerry@ska.ac.za>
2016-05-09 10:40:17 +0200

    Fix unit tests

--- katsdpingest/test/test_bf_ingest_server.py
+++ katsdpingest/test/test_bf_ingest_server.py
@@ -43,7 +43,7 @@ class BaseTestCaptureSession(object):
             cbf_spead=[endpoint.Endpoint('239.1.2.3', 7148)],
             file_base='/not_a_directory',
             buffer=self.buffer,
-            affinity=None, interface=None)
+            affinity=None, interface=None, telstate=None)
         self.loop = trollius.get_event_loop()
         self._spead_patcher = mock.patch.object(
             spead2.recv.trollius.Stream, 'add_udp_reader', add_buffer_reader)
