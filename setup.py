630cea1c10dd9d3b3a8ca35c1bfa6d9778c04326
Bruce Merry <bmerry@ska.ac.za>
2016-10-26 14:14:05 +0200

    Fix build when there isn't already a spead2 installed
    Previously it was picking up whatever common_features.h was installed
    system-wide. Now modified to run bootstrap and configure for spead2.

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages, Extension
+from distutils.command.build_ext import build_ext
 import glob
 import sys
+import subprocess
+import os.path
 import ctypes.util
 
 tests_require = ['mock', 'nose']
@@ -19,6 +22,28 @@ for name in bp_library_names:
 else:
     raise RuntimeError('Cannot find Boost.Python library')
 
+
+# Hack: this is copied and edited from spead2, so that we can run configure
+# inside the spead2 submodule.
+class BuildExt(build_ext):
+    def run(self):
+        self.mkpath(self.build_temp)
+        subprocess.check_call(['./bootstrap.sh'], cwd='spead2')
+        subprocess.check_call(os.path.abspath('spead2/configure'), cwd=self.build_temp)
+        # Ugly hack to add libraries conditional on configure result
+        have_ibv = False
+        with open(os.path.join(self.build_temp, 'include', 'spead2', 'common_features.h')) as f:
+            for line in f:
+                if line.strip() == '#define SPEAD2_USE_IBV 1':
+                    have_ibv = True
+        for extension in self.extensions:
+            if have_ibv:
+                extension.libraries.extend(['rdmacm', 'ibverbs'])
+            extension.include_dirs.insert(0, os.path.join(self.build_temp, 'include'))
+        # distutils uses old-style classes, so no super
+        build_ext.run(self)
+
+
 extensions = [
     Extension(
         '_bf_ingest_session',
@@ -29,9 +54,8 @@ extensions = [
         depends=glob.glob('spead2/src/*.h'),
         language='c++',
         include_dirs=['spead2/include'],
-        define_macros=[('SPEAD2_USE_IBV', '1')],
         extra_compile_args=['-std=c++11', '-g0'],
-        libraries=[bp_library, 'rdmacm', 'ibverbs', 'hdf5_cpp', 'hdf5', 'boost_system', 'boost_regex'])
+        libraries=[bp_library, 'hdf5_cpp', 'hdf5', 'boost_system', 'boost_regex'])
 ]
 
 setup(
@@ -43,6 +67,7 @@ setup(
     include_package_data=True,
     ext_package='katsdpingest',
     ext_modules=extensions,
+    cmdclass={'build_ext': BuildExt},
     scripts=[
         "scripts/ingest.py",
         "scripts/bf_ingest.py",
