b9f248c18643102dba9d86ce280025e2a57ab935
Bruce Merry <bmerry@ska.ac.za>
2018-07-20 11:28:28 +0200

    Remove a line of dead code

--- katsdpingest/katsdpingest/ingest_session.py
+++ katsdpingest/katsdpingest/ingest_session.py
@@ -980,7 +980,6 @@ class CBFIngest:
             raise ValueError('{0} is already in the active list of recipients'.format(endpoint))
         config = spead2.send.StreamConfig(max_packet_size=8872, rate=self.sd_spead_rate / 8)
         logger.info("Adding %s to signal display list. Starting stream...", endpoint)
-        extra_args = []
         if self.sd_spead_ifaddr is None:
             extra_args = {}
         else:
