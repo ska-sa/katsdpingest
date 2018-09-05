2368b6a6021e0620cd18a0157e4ac9bf19e42059
Bruce Merry <bmerry@ska.ac.za>
2018-09-05 14:49:43 +0200

    Turn off debugging in build again
    It was accidentally disabled in a previous commit.

--- katsdpbfingest/setup.py
+++ katsdpbfingest/setup.py
@@ -71,7 +71,7 @@ extensions = [
         language='c++',
         include_dirs=['spead2/include', 'spead2/3rdparty/pybind11/include'] + hdf5['include_dirs'],
         define_macros=hdf5['define_macros'],
-        extra_compile_args=['-std=c++11', '-O0', '-fvisibility=hidden'],
+        extra_compile_args=['-std=c++11', '-g0', '-fvisibility=hidden'],
         library_dirs=hdf5['library_dirs'],
         libraries=['boost_system', 'hdf5_cpp'] + hdf5['libraries']
     )
