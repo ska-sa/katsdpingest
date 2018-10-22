f7ea9e141bf3103e0d0c06fa4a7cd13ed4613a01
Bruce Merry <bmerry@ska.ac.za>
2018-10-22 10:33:07 +0200

    Add type annotation to _fix_descriptions

--- katsdpingest/katsdpingest/ingest_session.py
+++ katsdpingest/katsdpingest/ingest_session.py
@@ -147,7 +147,7 @@ def _split_array(x: np.ndarray, dtype) -> np.ndarray:
     return np.asarray(np.lib.stride_tricks.DummyArray(interface, base=x))
 
 
-def _fix_descriptions(desc):
+def _fix_descriptions(desc: Any) -> Any:
     """Massage operation descriptions to be suitable for telstate storage.
 
     It recursively:
