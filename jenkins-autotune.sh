4c230a9b598280500a61cee356068ce63b0075bd
Bruce Merry <bmerry@sarao.ac.za>
2024-03-01 11:47:22 +0200

    Push tuned images to a second registry
    This is to support the transition period while migrating from one to the
    other (see SPR1-200).

--- jenkins-autotune.sh
+++ jenkins-autotune.sh
@@ -15,5 +15,13 @@ COPY_FROM="$DOCKER_REGISTRY/katsdpingest_$GPU:latest"
 install_pinned.py -r requirements-autotune.txt
 docker pull "$BASE_IMAGE"
 docker pull "$COPY_FROM"
+trap "docker rmi $IMAGE" EXIT
 scripts/autotune_mkimage.py -H $DOCKER_HOST --tls --copy --copy-from "$COPY_FROM" "$IMAGE" "$BASE_IMAGE"
 docker push "$IMAGE"
+
+if [ -n "$DOCKER_REGISTRY2" ]; then
+    IMAGE2="$DOCKER_REGISTRY2/katsdpingest_$GPU:$LABEL"
+    trap "docker rmi $IMAGE2" EXIT
+    docker tag "$IMAGE" "$IMAGE2"
+    docker push "$IMAGE2"
+fi
