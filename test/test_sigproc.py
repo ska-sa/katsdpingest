d5ae99c7c5422422546d9f437818f5ee3b300cfb
Ludwig Schwardt <ludwig@ska.ac.za>
2026-03-11 13:30:39 +0200

    SPP1-198 Restore test_sigproc benchmark script
    This script was last significantly updated in June 2017 (see #181).
    In the meantime, continuum output became optional and we got a new weighting
    scheme and a new CAM flags interface, so the script doesn't work anymore.
    
    - Add a `--continuum` script argument that enables continuum output,
      needed for `IngestTemplate` initialisation.
    
    - Disable excision by default as that is the standard ingest setting now.
    
    - Fix the parameters to `IngestTemplate.instantiate` to include the flag
      counter channel range and the channel mask count.
    
    - Update to the latest set of input buffers, which replaces the old weight
      calculation buffers with flag-related buffers and signal display weights.
    
    - Ensure that the raw percentile ranges use plain old ints instead of
      np.int64, otherwise these incompatible types eventually crash `pycuda`
      inside `katsdpsigproc`.
    
    - Add some mild realism by setting a few random CAM flags in the channel
      mask and baseline flags.

--- test/test_sigproc.py
+++ test/test_sigproc.py
@@ -6,8 +6,16 @@ import katsdpsigproc.rfi.device as rfi
 import katsdpsigproc.accel as accel
 import katsdpingest.sigproc as sp
 from katsdpingest.utils import Range
+from katdal.flags import STATIC, CAM
 import numpy as np
 import argparse
+
+
+def add_random_flags(flags, flag_value, probability=0.1):
+    few_random_indices = np.random.choice(
+        flags.size, int(np.ceil(probability * flags.size)), replace=False
+    )
+    flags.flat[few_random_indices] |= np.uint8(flag_value)
 
 
 def generate_data(vis_in_device, channels, baselines):
@@ -35,7 +43,10 @@ def create_percentile_ranges(antennas):
         n_cross,           # crossvv
         2 * n_cross        # crosshv
     ]
-    cuts = np.cumsum([0] + sections)
+    # XXX Ensure that output has plain ints and not np.int64, as
+    # these numbers eventually pass via katsdpsigproc into pycuda,
+    # which cannot digest NumPy types.
+    cuts = np.cumsum([0] + sections).tolist()
     return [
         (cuts[0], cuts[2]),  # autohhvv
         (cuts[0], cuts[1]),  # autohh
@@ -51,7 +62,13 @@ def create_percentile_ranges(antennas):
 def create_template(context, args):
     percentile_ranges = create_percentile_ranges(args.mask_antennas)
     percentile_sizes = list(set([x[1] - x[0] for x in percentile_ranges]))
-    return sp.IngestTemplate(context, create_flagger(context, args), percentile_sizes, args.excise)
+    return sp.IngestTemplate(
+        context,
+        create_flagger(context, args),
+        percentile_sizes,
+        args.excise,
+        args.continuum,
+    )
 
 
 def main():
@@ -79,8 +96,10 @@ def main():
                         help='median filter kernel size (must be odd)')
     parser.add_argument('--sigmas', type=float, default=11.0,
                         help='threshold for detecting RFI')
-    parser.add_argument('--no-excise', dest='excise', action='store_false',
-                        help='disable excision of flagged data')
+    parser.add_argument('--excise', action='store_true',
+                        help='enable excision of flagged data')
+    parser.add_argument('--continuum', action='store_true',
+                        help='enable continuum output')
     parser.add_argument('--repeat', '-r', type=int, default=8,
                         help='number of dumps to process')
     parser.add_argument('--no-transfer', '-N', action='store_true',
@@ -89,48 +108,69 @@ def main():
     args = parser.parse_args()
     channels = args.channels + args.border
     channel_range = Range(args.border // 2, args.channels + args.border // 2)
+    kept_channels = len(channel_range)
+    count_flags_channel_range = Range(
+        channel_range.start - args.border // 4,
+        channel_range.stop + args.border // 4,
+    )
     cbf_baselines = args.antennas * (args.antennas + 1) * 2
     if args.mask_antennas is None:
         args.mask_antennas = args.antennas
     baselines = args.mask_antennas * (args.mask_antennas + 1) * 2
+    masks = 3
 
     context = accel.create_some_context(True)
     command_queue = context.create_command_queue(profile=True)
     template = create_template(context, args)
     proc = template.instantiate(
-        command_queue, channels, channel_range, 2 * args.mask_antennas,
-        cbf_baselines, baselines,
-        args.freq_avg, args.sd_freq_avg, create_percentile_ranges(args.mask_antennas),
-        threshold_args={'n_sigma': args.sigmas})
+        command_queue,
+        channels,
+        channel_range,
+        count_flags_channel_range,
+        cbf_baselines,
+        baselines,
+        masks,
+        cont_factor=args.freq_avg,
+        sd_cont_factor=args.sd_freq_avg,
+        percentile_ranges=create_percentile_ranges(args.mask_antennas),
+        threshold_args={'n_sigma': args.sigmas}
+    )
     print("{0} bytes required".format(proc.required_bytes()))
     proc.ensure_all_bound()
 
     permutation = np.random.permutation(baselines).astype(np.int16)
     permutation = np.r_[permutation, -np.ones(cbf_baselines - baselines, np.int16)]
-    # The baseline_inputs and input_auto_baseline arrays aren't consistent
-    # with the percentile ranges, but that doesn't really matter (although
-    # it may impact memory access patterns and hence performance).
-    baseline_inputs = []
-    input_auto_baseline = np.zeros(2 * args.mask_antennas, np.uint16)
-    for i in range(2 * args.mask_antennas):
-        for j in range(i // 2 * 2, 2 * args.mask_antennas):
-            if i == j:
-                input_auto_baseline[i] = len(baseline_inputs)
-            baseline_inputs.append((i, j))
-    assert len(baseline_inputs) == baselines
-    baseline_inputs = np.array(baseline_inputs, np.uint16)
+    channel_mask = np.zeros((masks, channels), np.uint8)
+    channel_mask[:, :args.border + 2] |= np.uint8(STATIC)
+    channel_mask[:, -args.border - 2:] |= np.uint8(STATIC)
+    add_random_flags(channel_mask, CAM)
+    channel_mask_idx = np.random.randint(0, masks, baselines).astype(np.uint32)
+    baseline_flags = np.zeros((baselines,), np.uint8)
+    add_random_flags(baseline_flags, CAM, probability=0.01)
+    timeseries_weights = np.random.choice(2, size=kept_channels, p=[0.1, 0.9])
+    timeseries_weights = timeseries_weights.astype(np.float32)
+    timeseries_weights /= np.sum(timeseries_weights)
+
     proc.buffer('permutation').set(command_queue, permutation)
-    proc.buffer('input_auto_baseline').set(command_queue, input_auto_baseline)
-    proc.buffer('baseline_inputs').set(command_queue, baseline_inputs)
+    proc.buffer('channel_mask').set(command_queue, channel_mask)
+    proc.buffer('channel_mask_idx').set(command_queue, channel_mask_idx)
+    proc.buffer('baseline_flags').set(command_queue, baseline_flags)
+    proc.buffer('timeseries_weights').set(command_queue, timeseries_weights)
 
     command_queue.finish()
 
     vis_in_device = proc.buffer('vis_in')
-    output_names = ['spec_vis', 'spec_weights', 'spec_weights_channel', 'spec_flags',
-                    'cont_vis', 'cont_weights', 'cont_weights_channel', 'cont_flags']
+    output_names = ['spec_vis', 'spec_weights', 'spec_weights_channel', 'spec_flags']
+    if args.continuum:
+        output_names.extend(['cont_vis', 'cont_weights', 'cont_weights_channel', 'cont_flags'])
     output_buffers = [proc.buffer(name) for name in output_names]
     output_arrays = [buf.empty_like() for buf in output_buffers]
-    sd_names = ['sd_cont_vis', 'sd_cont_flags', 'sd_cont_weights', 'timeseries', 'timeseriesabs']
+    sd_names = ['sd_spec_vis', 'sd_spec_flags', 'sd_spec_weights', 'sd_spec_weights_channel',
+                'sd_flag_counts', 'sd_flag_any_counts', 'timeseries', 'timeseriesabs']
+    if args.continuum:
+        sd_names.extend(
+            ['sd_cont_vis', 'sd_cont_flags', 'sd_cont_weights', 'sd_cont_weights_channel']
+        )
     for i in range(5):
         sd_names.append('percentile{0}'.format(i))
     sd_buffers = [proc.buffer(name) for name in sd_names]
