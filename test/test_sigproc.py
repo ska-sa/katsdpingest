c3fc98605e425cdb1d998f1aff50ead961a0c30c
Ludwig Schwardt <ludwig@ska.ac.za>
2026-03-18 21:56:00 +0200

    SPP1-198 Modernise script
    Pick better variable names and script arguments. Replace Range with the
    newer ChannelRanges. Improve docstrings and script output.

--- test/test_sigproc.py
+++ test/test_sigproc.py
@@ -9,8 +9,23 @@ import katsdpsigproc.rfi.device as rfi
 import numpy as np
 from katdal.flags import CAM, STATIC
 
+from katsdpingest.ingest_session import ChannelRanges
 import katsdpingest.sigproc as sp
 from katsdpingest.utils import Range
+
+
+def create_channel_ranges(args):
+    return ChannelRanges(
+        servers=args.servers,
+        server_id=args.server_id - 1,
+        channels=args.cbf_channels,
+        cont_factor=args.continuum_factor,
+        sd_cont_factor=args.sd_continuum_factor,
+        streams=args.servers,  # not used, so pretend there is one stream per server
+        guard=args.guard_channels,
+        all_output=Range(0, args.cbf_channels),
+        all_sd_output=Range(0, args.cbf_channels),
+    )
 
 
 def add_random_flags(flags, flag_value, probability=0.1):
@@ -27,10 +42,12 @@ def generate_data(vis_in_device, channels, baselines):
 
 
 def create_flagger(context, args):
+    channel_ranges = create_channel_ranges(args)
+    input_channels = len(channel_ranges.input)
     background = rfi.BackgroundMedianFilterDeviceTemplate(
-        context, args.width)
-    noise_est = rfi.NoiseEstMADTDeviceTemplate(
-        context, args.channels + args.border)
+        context, args.flagger_width, use_flags=rfi.BackgroundFlags.FULL
+    )
+    noise_est = rfi.NoiseEstMADTDeviceTemplate(context, input_channels)
     threshold = rfi.ThresholdSumDeviceTemplate(context)
     return rfi.FlaggerDeviceTemplate(background, noise_est, threshold)
 
@@ -62,7 +79,7 @@ def create_percentile_ranges(antennas):
 
 
 def create_template(context, args):
-    percentile_ranges = create_percentile_ranges(args.mask_antennas)
+    percentile_ranges = create_percentile_ranges(args.output_antennas)
     percentile_sizes = list(set([x[1] - x[0] for x in percentile_ranges]))
     return sp.IngestTemplate(
         context,
@@ -74,82 +91,116 @@ def create_template(context, args):
 
 
 def main():
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    description = """
+    Benchmark the ingest pipeline on generated correlator data that
+    strips out the network layer. The **input** parameters are associated
+    with the CBF SPEAD stream, while the **output** parameters are
+    associated with the SDP L0 output products and signal display ("sd")
+    streams.
+    """
+    parser = argparse.ArgumentParser(
+        description=description,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument_group('Data selection')
-    parser.add_argument('--antennas', '-a', type=int, default=7,
-                        help='total number of antennas',)
-    parser.add_argument('--mask-antennas', type=int, default=None,
-                        help='number of antennas in antenna mask')
-    parser.add_argument('--channels', '-c', type=int, default=1024,
-                        help='number of frequency channels')
-    parser.add_argument('--border', '-B', type=int, default=0, help='extra overlap channels')
+    parser.add_argument_group('Data size setup')
+    parser.add_argument('--input-antennas', '-a', type=int, default=7,
+                        help='total number of antennas in CBF input stream')
+    parser.add_argument('--output-antennas', type=int, default=None,
+                        help='number of antennas in SDP output products (keeps all by default)')
+    parser.add_argument('--cbf-channels', '-c', type=int, default=1024,
+                        help='number of frequency channels in CBF input stream')
+    parser.add_argument('--servers', type=int, default=1,
+                        help='number of intended parallel ingest servers (only simulate one)')
 
     parser.add_argument_group('Parameters')
-    parser.add_argument('--time-avg', '-T', type=int, default=4,
-                        help='number of input dumps per output dump')
-    parser.add_argument('--freq-avg', '-F', type=int, default=16,
-                        help='number of input channels per continuum channel')
-    parser.add_argument('--sd-time-avg', type=int, default=4,
-                        help='number of input dumps per signal display dump')
-    parser.add_argument('--sd-freq-avg', type=int, default=128,
-                        help='number of input channels for signal display channel')
-    parser.add_argument('--width', '-w', type=int, default=13,
-                        help='median filter kernel size (must be odd)')
-    parser.add_argument('--sigmas', type=float, default=11.0,
-                        help='threshold for detecting RFI')
+    parser.add_argument('--server-id', type=int, default=None,
+                        help='index of this server amongst parallel servers (1-based)')
+    parser.add_argument('--input-int-time', default=0.5, type=float,
+                        help='seconds between input CBF dumps')    
+    parser.add_argument('--output-int-time', default=2.0, type=float,
+                        help='seconds between output SDP dumps')
+    parser.add_argument('--sd-int-time', default=2.0, type=float,
+                        help='seconds between signal display updates')
+    parser.add_argument('--continuum-factor', '-F', type=int, default=16,
+                        help='number of input CBF channels per continuum output SDP channel')
+    parser.add_argument('--sd-continuum-factor', type=int, default=128,
+                        help='number of input CBF channels per signal display channel')
+    parser.add_argument('--guard-channels', '-B', type=int, default=64,
+                        help='number of extra input channels on either side used by flagger')
+    parser.add_argument('--flagger-width', '-w', type=int, default=13,
+                        help='median filter kernel size in ingest RFI flagger (must be odd)')
+    parser.add_argument('--flagger-sigma', type=float, default=11.0,
+                        help='threshold for detecting RFI in ingest RFI flagger')
     parser.add_argument('--excise', action='store_true',
                         help='enable excision of flagged data')
     parser.add_argument('--continuum', action='store_true',
                         help='enable continuum output')
-    parser.add_argument('--repeat', '-r', type=int, default=8,
-                        help='number of dumps to process')
+    parser.add_argument('--repeat', '-r', type=int, default=None,
+                        help='number of input CBF dumps to process (default is 2 SDP dumps)')
     parser.add_argument('--no-transfer', '-N', action='store_true',
                         help='skip data transfers')
 
     args = parser.parse_args()
-    channels = args.channels + args.border
-    channel_range = Range(args.border // 2, args.channels + args.border // 2)
-    kept_channels = len(channel_range)
-    count_flags_channel_range = Range(
-        channel_range.start - args.border // 4,
-        channel_range.stop + args.border // 4,
-    )
-    cbf_baselines = args.antennas * (args.antennas + 1) * 2
-    if args.mask_antennas is None:
-        args.mask_antennas = args.antennas
-    baselines = args.mask_antennas * (args.mask_antennas + 1) * 2
-    masks = 3
+    if args.server_id is None:
+        args.server_id = 3 if args.servers == 4 else 1
+    if args.output_antennas is None:
+        args.output_antennas = args.input_antennas
+    dumps_per_output_dump = max(1, int(round(args.output_int_time / args.input_int_time)))
+    dumps_per_sd_dump = max(1, int(round(args.sd_int_time / args.input_int_time)))
+    if args.repeat is None:
+        args.repeat = max(2 * dumps_per_output_dump, 2 * dumps_per_sd_dump)
+
+    channel_ranges = create_channel_ranges(args)
+    input_channels = len(channel_ranges.input)
+    output_channel_range = channel_ranges.computed.relative_to(channel_ranges.input)
+    sd_channel_range = channel_ranges.sd_output.relative_to(channel_ranges.input)
+    output_channels = len(output_channel_range)
+    input_baselines = args.input_antennas * (args.input_antennas + 1) * 2
+    output_baselines = args.output_antennas * (args.output_antennas + 1) * 2
+    masks = 2
+    percentile_ranges = create_percentile_ranges(args.output_antennas)
+
+    print(f"Total number of CBF frequency channels: {len(channel_ranges.cbf)}")
+    print(f"This is meant to be split across {args.servers} ingest server(s)")
+    print(f"This simulated server processes channels {channel_ranges.input}")
+    print(f"Number of channels: input={input_channels}, output={output_channels}")
+    print(f"Number of baselines: input={input_baselines}, output={output_baselines}")
+    print(f"Number of input dumps per output dump: {dumps_per_output_dump}")
+    print(f"Number of input dumps per signal display dump: {dumps_per_sd_dump}")
 
     context = accel.create_some_context(True)
     command_queue = context.create_command_queue(profile=True)
     template = create_template(context, args)
     proc = template.instantiate(
         command_queue,
-        channels,
-        channel_range,
-        count_flags_channel_range,
-        cbf_baselines,
-        baselines,
+        input_channels,
+        output_channel_range,
+        sd_channel_range,
+        input_baselines,
+        output_baselines,
         masks,
-        cont_factor=args.freq_avg,
-        sd_cont_factor=args.sd_freq_avg,
-        percentile_ranges=create_percentile_ranges(args.mask_antennas),
-        threshold_args={'n_sigma': args.sigmas}
+        channel_ranges.cont_factor,
+        channel_ranges.sd_cont_factor,
+        percentile_ranges,
+        threshold_args={'n_sigma': args.flagger_sigma}
     )
-    print("{0} bytes required".format(proc.required_bytes()))
+    print(f"Total device buffer size = {proc.required_bytes()/1e6:.6f} MB")
     proc.ensure_all_bound()
 
-    permutation = np.random.permutation(baselines).astype(np.int16)
-    permutation = np.r_[permutation, -np.ones(cbf_baselines - baselines, np.int16)]
-    channel_mask = np.zeros((masks, channels), np.uint8)
-    channel_mask[:, :args.border + 2] |= np.uint8(STATIC)
-    channel_mask[:, -args.border - 2:] |= np.uint8(STATIC)
+    permutation = np.random.permutation(output_baselines).astype(np.int16)
+    permutation = np.r_[permutation, -np.ones(input_baselines - output_baselines, np.int16)]
+    # The channel mask combines multiple static masks and the channel_data_suspect sensor
+    channel_mask = np.zeros((masks, input_channels), np.uint8)
+    channel_mask[:, :input_channels // 10] |= np.uint8(STATIC)
+    channel_mask[:, -input_channels // 10:] |= np.uint8(STATIC)
     add_random_flags(channel_mask, CAM)
-    channel_mask_idx = np.random.randint(0, masks, baselines).astype(np.uint32)
-    baseline_flags = np.zeros((baselines,), np.uint8)
+    channel_mask_idx = np.random.randint(0, masks, output_baselines).astype(np.uint32)
+    # The baseline flags combine the CAM per-dish data_suspect sensor and input_data_suspect
+    baseline_flags = np.zeros((output_baselines,), np.uint8)
     add_random_flags(baseline_flags, CAM, probability=0.01)
-    timeseries_weights = np.random.choice(2, size=kept_channels, p=[0.1, 0.9])
+    # The timeseries weights / mask are set by the signal display controls via telstate
+    timeseries_weights = np.random.choice(2, size=output_channels, p=[0.1, 0.9])
     timeseries_weights = timeseries_weights.astype(np.float32)
     timeseries_weights /= np.sum(timeseries_weights)
 
@@ -173,30 +224,35 @@ def main():
         sd_names.extend(
             ['sd_cont_vis', 'sd_cont_flags', 'sd_cont_weights', 'sd_cont_weights_channel']
         )
-    for i in range(5):
-        sd_names.append('percentile{0}'.format(i))
+    for i in range(len(percentile_ranges)):
+        sd_names.append(f"percentile{i}")
     sd_buffers = [proc.buffer(name) for name in sd_names]
     sd_arrays = [buf.empty_like() for buf in sd_buffers]
 
-    dumps = [generate_data(vis_in_device, channels, cbf_baselines)
-             for i in range(max(args.sd_time_avg, args.time_avg))]
+    print("Generating input visibility data...")
+    dumps = [generate_data(vis_in_device, input_channels, input_baselines)
+             for _ in range(max(dumps_per_sd_dump, dumps_per_output_dump))]
     # Push data before we start timing, to ensure everything is allocated
     for dump in dumps:
         proc.buffer('vis_in').set(command_queue, dump)
 
+    output_dumps = args.repeat // dumps_per_output_dump
+    print(f"Processing {args.repeat} CBF dumps ({output_dumps} SDP dumps)...")
     start_event = command_queue.enqueue_marker()
     proc.start_sum()
+    proc.start_sd_sum()
+    # Iterate over input CBF dumps
     for pass_ in range(args.repeat):
         if not args.no_transfer:
             proc.buffer('vis_in').set_async(command_queue, dumps[pass_ % len(dumps)])
         proc()
-        if (pass_ + 1) % args.time_avg == 0:
+        if (pass_ + 1) % dumps_per_output_dump == 0:
             proc.end_sum()
             if not args.no_transfer:
                 for buf, array in zip(output_buffers, output_arrays):
                     buf.get_async(command_queue, array)
             proc.start_sum()
-        if (pass_ + 1) % args.sd_time_avg == 0:
+        if (pass_ + 1) % dumps_per_sd_dump == 0:
             proc.end_sd_sum()
             if not args.no_transfer:
                 for buf, array in zip(sd_buffers, sd_arrays):
@@ -205,7 +261,9 @@ def main():
     end_event = command_queue.enqueue_marker()
     elapsed_ms = end_event.time_since(start_event) * 1000.0
     dump_ms = elapsed_ms / args.repeat
-    print("{0:.3f}ms ({1:.3f}ms per dump)".format(elapsed_ms, dump_ms))
+    realtime_perc = dump_ms / 1000.0 / args.input_int_time * 100.0
+    print(f"Total runtime = {elapsed_ms:.3f} ms ({dump_ms:.3f} ms per input CBF dump)")
+    print(f"Processing takes {realtime_perc:.1f}% of real time")
 
 
 if __name__ == '__main__':
